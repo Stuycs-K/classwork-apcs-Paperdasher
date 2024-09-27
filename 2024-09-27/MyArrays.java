c0e573033d54aded54d54db1a2178140a884f0f2
Sean Takahashi <seanyuto@gmail.com>
2024-09-27 11:01:30 -0400

    started writing second func, need to clean up test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -15,11 +15,14 @@ public class MyArrays{
 
 
   public static int[] concatArray(int[]ary1,int[]ary2){
-
+    int [] concat_ary = new int[ary1.length + ary2.length];
+    for (int i = 0; i < concat_ary.length; i++)
   }
 
   public static void main(String[] args){
-
+    int [] ary = new int[] {5, 3, 10, 24, 9, 6};
+    System.out.println(returnCopy(ary) == arr);
+    System.out.println(concatArray(ary, ));
   }
 
 }
