312e98870d69b0ef96248adab19883eae267db73
Sean Takahashi <seanyuto@gmail.com>
2024-09-27 10:52:26 -0400

    returnCopy written

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,25 @@
+public class MyArrays{
+  public static int[] returnCopy(int[]ary){
+    String array_string = "";
+    for (int i = 0; i < ary.length; i++){
+      if (i == arr.length - 1){
+      array_string += nums[i];
+      }
+      else{
+        array_string += nums[i] + ", ";
+      }
+    }
+    array_string += "]";
+    return array_string;
+  }
+
+
+  public static int[] concatArray(int[]ary1,int[]ary2){
+
+  }
+
+  public static void main(String[] args){
+
+  }
+
+}
