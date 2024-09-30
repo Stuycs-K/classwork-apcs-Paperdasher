055fe7e02906d91f2828f3ccd4e46afef4e42f01
Sean Takahashi <seanyuto@gmail.com>
2024-09-30 01:08:49 -0400

    finished code

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,28 +1,50 @@
 public class MyArrays{
-  public static int[] returnCopy(int[]ary){
-    String array_string = "";
+  public static String arrayTostring(int[] ary){
+    String array_string = "[";
     for (int i = 0; i < ary.length; i++){
-      if (i == arr.length - 1){
-      array_string += nums[i];
+      if (i == ary.length - 1){
+      array_string += ary[i];
       }
       else{
-        array_string += nums[i] + ", ";
+        array_string += ary[i] + ", ";
       }
     }
     array_string += "]";
     return array_string;
   }
+  
+  public static int[] returnCopy(int[]ary){
+    int [] copy_ary = new int[ary.length];
+    for (int i = 0; i < ary.length; i++){
+      copy_ary[i] = ary[i];
+    }
+    return copy_ary;
+  }
 
 
   public static int[] concatArray(int[]ary1,int[]ary2){
     int [] concat_ary = new int[ary1.length + ary2.length];
-    for (int i = 0; i < concat_ary.length; i++)
+    for (int i = 0; i < ary1.length; i++){
+      concat_ary[i] = ary1[i];
+    }
+    for (int i = 0; i < ary2.length; i++){
+      concat_ary[i + ary1.length] = ary2[i];
+    }
+    return concat_ary;
   }
 
   public static void main(String[] args){
-    int [] ary = new int[] {5, 3, 10, 24, 9, 6};
-    System.out.println(returnCopy(ary) == arr);
-    System.out.println(concatArray(ary, ));
+    int [] ary = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+    int [] ary2 = new int[] {9, 10, 11, 12, 13};
+
+    System.out.println(returnCopy(ary) == ary);
+    
+    System.out.println(arrayTostring(ary));
+    
+    System.out.println(arrayTostring(ary2));
+
+    System.out.println(arrayTostring(concatArray(ary, ary2)));
+
   }
 
 }
