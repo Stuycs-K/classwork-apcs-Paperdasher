27fe4741dcd5fd771df23f27b2b17e0a96241be7
Sean Takahashi <seanyuto@gmail.com>
2024-09-30 01:11:22 -0400

    additional test cases, all passed

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -37,6 +37,12 @@ public class MyArrays{
     int [] ary = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
     int [] ary2 = new int[] {9, 10, 11, 12, 13};
 
+    int [] ary3 = new int[] {};
+    int [] ary4 = new int[] {};
+
+    int [] ary5 = new int[5];
+    int [] ary6 = new int[2];
+
     System.out.println(returnCopy(ary) == ary);
     
     System.out.println(arrayTostring(ary));
@@ -44,6 +50,24 @@ public class MyArrays{
     System.out.println(arrayTostring(ary2));
 
     System.out.println(arrayTostring(concatArray(ary, ary2)));
+
+    
+    System.out.println(returnCopy(ary3) == ary3);
+    
+    System.out.println(arrayTostring(ary3));
+    
+    System.out.println(arrayTostring(ary4));
+
+    System.out.println(arrayTostring(concatArray(ary3, ary4)));
+
+    
+    System.out.println(returnCopy(ary6) == ary6);
+    
+    System.out.println(arrayTostring(ary5));
+    
+    System.out.println(arrayTostring(ary6));
+
+    System.out.println(arrayTostring(concatArray(ary5, ary6)));
 
   }
 
