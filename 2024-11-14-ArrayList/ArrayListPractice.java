536ebfd1a9082e0eb172bc91350e8fd4bb710f37
Sean Takahashi <seanyuto@gmail.com>
2024-11-14 10:39:45 -0500

    wrote createarraylist method and tested in new Driver file

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class ArrayListPractice{
   public static ArrayList<String>createRandomArray(int size){
-    
+    ArrayList<String> randomarray = new ArrayList<String>(size);
+    for (int i = 0; i < size; i++){
+      int temp = (int)(Math.random() * 11);
+      String Temp = "";
+      if (temp != 0){
+        Temp += temp;
+      }
+      randomarray.add(Temp);
+    }
+    return randomarray;
   }
 }
---
+++ 2024-11-14-ArrayList/Driver.java
@@ -0,0 +1,11 @@
+public class Driver{
+  public static void main(String[] args){
+    //testing createRandomArray
+    System.out.println(ArrayListPractice.createRandomArray(0));
+    System.out.println(ArrayListPractice.createRandomArray(10));
+    System.out.println(ArrayListPractice.createRandomArray(1));
+    System.out.println(ArrayListPractice.createRandomArray(50));
+
+    
+  }
+}
