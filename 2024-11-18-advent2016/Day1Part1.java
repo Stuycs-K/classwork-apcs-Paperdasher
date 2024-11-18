9111023b27d75a59bbe38da0e1fe3070f83a4456
Sean Takahashi <seanyuto@gmail.com>
2024-11-18 11:00:17 -0500

    wrote tentative code, need to add testing

---
+++ 2024-11-18-advent2016/Day1Part1.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1Part1{
+  Scanner sc1 = new Scanner("Day1Part1Input.txt");
+  Scanner sc2 = new Scanner("Day1Part1Input.txt");
+
+  public static int blocks_away(){
+    int x_corr = 0, y_corr = 0;
+    int angle_dir = 0;
+    int blocks = 0;
+
+    while(sc1.hasNext()){
+      if (sc1.next().contains("L")){
+        angle_dir -= 90;
+      }
+      else{
+        angle_dir += 90;
+      }
+
+      if ((angle_dir >= 360) || (angle_dir <= -360)){
+        (angle_dir - ((angle_dir / 360) * 360));
+      }
+
+      if (angle_dir == 0){
+        y_corr += sc2.nextInt();
+      }
+      if (angle_dir == 90 || angle_dir == -270){
+        x_corr += sc2.nextInt();
+      }
+      if (angle_dir == 180 || angle_dir == -180){
+        y_corr -= sc2.nextInt();
+      }
+      if (angle_dir == 270 || angle_dir == -90){
+        x_corr -= sc2.nextInt();
+      }
+
+      blocks = Math.abs(x_corr) + Math.abs(y_corr);
+      return blocks;
+    }
+
+
+
+
+
+    return blocks;
+  }
+}
