41041bbae0d9f4d24f9508beb97ff788f7267621
Sean Takahashi <seanyuto@gmail.com>
2024-11-19 00:46:45 -0500

    added input text file, basic starter code

---
+++ 2024-11-18-advent2016/Day2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day2 {
+
+    public static void main(String[] args){
+        Bathroom("Day2Input.txt");
+    }
+
+    public static int Bathroom(String input){
+        try{
+          File file = new File(input);
+          Scanner sc = new Scanner(file);  
+        }
+    }
+}
