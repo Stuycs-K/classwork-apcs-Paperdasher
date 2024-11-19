43683532cac24bfbf125ce29c6871c5ddae8a423
Sean Takahashi <seanyuto@gmail.com>
2024-11-19 10:56:43 -0500

    wrote code to take in file and convert to string array

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -3,15 +3,27 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Day2 {
+  public static void main(String[] args){
+      Bathroom("Day2Input.txt");
+  }
 
-    public static void main(String[] args){
-        Bathroom("Day2Input.txt");
+  public static int Bathroom(String input){
+
+    String[] data = null;
+    try{
+      File file = new File(input);
+      Scanner sc = new Scanner(file);
+      for (int i = 0; sc.hasNextLine(); i++){
+        data[i] = sc.nextLine();
+      }
+      sc.close();
+    } catch(FileNotFoundException e){
+      System.out.println("File not found");
     }
 
-    public static int Bathroom(String input){
-        try{
-          File file = new File(input);
-          Scanner sc = new Scanner(file);  
-        }
-    }
+    String[][] keypad = new String[3][3];
+
+    
+
+  }
 }
