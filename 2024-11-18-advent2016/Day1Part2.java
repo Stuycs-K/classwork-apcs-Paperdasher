1d68467e854cbc294120d52c8017791d2f19356e
Sean Takahashi <seanyuto@gmail.com>
2024-11-22 11:44:06 -0500

    added basic scanner code, wrote output statement for main function

--- 2024-11-18-advent2016/Day1Part2.java
+++ 2024-11-18-advent2016/Day1Part2.java
@@ -3,4 +3,20 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Day1Part2 {
+    public static void main(String[] args){
+        System.out.println(reachtwice("Day1Part1Input.txt"));
+    }
+
+    public static int reachtwice(String filename){
+        try{
+            File file = new File(filename);
+            Scanner sc = new Scanner(file);
+            String data = sc.nextLine();
+            datainput = data.split(", ");
+            sc.close();  
+          } catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+
+    }
 }
