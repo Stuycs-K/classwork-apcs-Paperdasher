67e4cb854f4d44fe3dd796d5966c5abff54e299e
Sean Takahashi <seanyuto@gmail.com>
2024-11-24 15:40:46 -0500

    finished part2 of day1, answer correct

--- 2024-11-18-advent2016/Day1Part2.java
+++ 2024-11-18-advent2016/Day1Part2.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Day1Part2 {
     public static void main(String[] args){
@@ -8,15 +9,51 @@ public class Day1Part2 {
     }
 
     public static int reachtwice(String filename){
+        int x_corr = 0, y_corr = 0;
+        int angle_dir = 0, blocks = 0;
+
         try{
             File file = new File(filename);
             Scanner sc = new Scanner(file);
-            String data = sc.nextLine();
-            datainput = data.split(", ");
-            sc.close();  
+            ArrayList<String> visited = new ArrayList<String>();
+
+            while(sc.hasNext()){
+                String direction = sc.next();
+                if (direction.substring(0, 1).equals("L")){
+                    angle_dir--;
+                }
+                else{
+                    angle_dir++;
+                }
+
+                if ((angle_dir >= 4) || (angle_dir <= -4)){
+                    angle_dir = (angle_dir + 4) % 4;
+                }
+
+                blocks = Integer.parseInt(direction.substring(1, direction.length() - 1));
+                for (int i = 0; i < blocks; i++){
+                    if (angle_dir == 0){
+                        y_corr++;
+                    }
+                    if (angle_dir == 1 || angle_dir == -3){
+                        x_corr++;
+                    }
+                    if (angle_dir == 2|| angle_dir == -2){
+                        y_corr--;
+                    }
+                    if (angle_dir == 3 || angle_dir == -1){
+                        x_corr--;
+                    }
+                    String location = x_corr + ", " + y_corr;
+                    if (visited.contains(location)){
+                        return Math.abs(x_corr) + Math.abs(y_corr);
+                    }
+                    visited.add(location);
+                }
+            }
           } catch(FileNotFoundException e){
             System.out.println("File not found");
         }
-
+        return -1;
     }
 }
