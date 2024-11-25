bdd9a5fa390390c42dc5f617c01b6923cbe45b12
Sean Takahashi <seanyuto@gmail.com>
2024-11-25 02:34:06 -0500

    added part2 code, changed max to min, output is correct

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Day6 {
     public static void main(String[] args){
         System.out.println(message("Day6Input.txt"));
+        System.out.println(message2("Day6Input.txt"));
     }
 
     public static String message(String inputfile){
@@ -36,6 +37,42 @@ public class Day6 {
                     }
                 }
                 output += Character.toString((char)(97 + maxIndex));
+            }
+            return output;
+        } catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        return null;
+    }
+
+    public static String message2(String inputfile){
+        int[][] freq = new int[8][26];
+        int tempnum;
+        String current = "";
+        try{
+            File input = new File(inputfile);
+            Scanner sc = new Scanner(input);
+
+            String output = "";
+
+            for (int i = 0; sc.hasNextLine(); i++){
+                current = sc.nextLine();
+                for (int j = 0; j < 8; j++){
+                    tempnum = current.charAt(j) - 'a';
+                    freq[j][tempnum]++;
+                }
+            }
+
+            int min = 500, minIndex = 500;
+            for (int i = 0; i < 8; i++){
+                min = 500; minIndex = 500;
+                for (int j = 0; j < 26; j++){
+                    if (min > freq[i][j]){
+                        min = freq[i][j];
+                        minIndex = j;
+                    }
+                }
+                output += Character.toString((char)(97 + minIndex));
             }
             return output;
         } catch(FileNotFoundException e){
