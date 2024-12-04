0cf5d2ea6d7b1522e6679fe8ee19ee6463ab6979
Sean Takahashi <seanyuto@gmail.com>
2024-12-04 01:34:12 -0500

    changed background color

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -2,15 +2,23 @@ public class ColorDemo{
   public static final String RESET = "\u001b[0m";
   public static int BOLD = 1;
   public static void main(String[] args){
-    System.out.print("\u001b[1;92;43m");
-    System.out.println("cccccc    ssssss");
-    System.out.println("cccccc    sss   ");
-    System.out.println("cc        sss   ");
-    System.out.println("cc        ssssss");
-    System.out.println("cc           sss");
-    System.out.println("cc           sss");
-    System.out.println("cccccc    ssssss");
-    System.out.println("cccccc    ssssss");
+    System.out.print("\u001b[1;30m");
+    System.out.print("\u001b[48;2;"+0+";"+255+";"+0+"m");
+    System.out.println("......    ......");
+    System.out.print("\u001b[48;2;"+30+";"+255+";"+30+"m");
+    System.out.println("......    ...   ");
+    System.out.print("\u001b[48;2;"+60+";"+255+";"+60+"m");
+    System.out.println("...       ...   ");
+    System.out.print("\u001b[48;2;"+90+";"+255+";"+90+"m");
+    System.out.println("..        ......");
+    System.out.print("\u001b[48;2;"+120+";"+255+";"+120+"m");
+    System.out.println("..           ...");
+    System.out.print("\u001b[48;2;"+150+";"+255+";"+150+"m");
+    System.out.println("..           ...");
+    System.out.print("\u001b[48;2;"+180+";"+255+";"+180+"m");
+    System.out.println("......    ......");
+    System.out.print("\u001b[48;2;"+210+";"+255+";"+210+"m");
+    System.out.println("......    ......");
 
 
 
