393d9196dd077d20a27a7445b7e13b67c55fda54
Sean Takahashi <seanyuto@gmail.com>
2024-12-04 01:18:38 -0500

    wrote output message of CS in green color

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,26 +1,19 @@
 public class ColorDemo{
-  public static void main(String args[]){
-    for(int r = 0; r < 256; r+=32){
-      for(int g = 0; g <= 256; g+=32){
-        for(int b = 0; b <= 256; b+=32){
-          System.out.print("\u001b[1;38;2;"+r+";"+g+";"+b+";7m.");
-        }
-      }
-      System.out.println();
-    }
-    System.out.print("\u001b[0m");
+  public static final String RESET = "\u001b[0m";
+  public static int BOLD = 1;
+  public static void main(String[] args){
+    System.out.print("\u001b[1;92;43m");
+    System.out.println("cccccc    ssssss");
+    System.out.println("cccccc    sss   ");
+    System.out.println("cc        sss   ");
+    System.out.println("cc        ssssss");
+    System.out.println("cc           sss");
+    System.out.println("cc           sss");
+    System.out.println("cccccc    ssssss");
+    System.out.println("cccccc    ssssss");
+
+
+
+    System.out.print(RESET);
   }
-
-  public static void sleep(int milli){
-        try{
-                Thread.sleep(milli);
-        }catch(Exception e){
-        }
-  }
-
-  public static void color(int foreground,int background){
-    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
-  }
-
-
 }
