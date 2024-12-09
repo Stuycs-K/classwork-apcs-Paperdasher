d225ba242f92dd39ae300c0f34cc32d839c7b364
Sean Takahashi <seanyuto@gmail.com>
2024-12-09 02:33:05 -0500

    did adequate testing with multiple turns and using all methods, passed

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,7 +1,43 @@
 public class Driver{
     public static void main(String[] args){
-      Adventurer p1 = new Warrior("John", 20);
-      //test attack, support(self and other), specialAttack
-      //test Mana constructor
+      Adventurer p1 = new Warrior("A", 100, 4);
+      Adventurer p2 = new Warrior("B", 100, 20);
+      
+      System.out.println("\n\n------THE BATTLE BEGINS!------");
+  
+      System.out.println("\nTURN 1");
+      System.out.println(p1.attack(p2));
+      System.out.println(p2.attack(p1));
+
+      System.out.println("\nTURN 2");
+      System.out.println(p1.support());
+      System.out.println(p2.support(p1));
+
+      System.out.println("\nTURN 3");
+      System.out.println(p1.attack(p2));
+      System.out.println(p2.support());
+
+      System.out.println("\nTURN 4");
+      System.out.println(p1.specialAttack(p2));
+      System.out.println(p2.specialAttack(p1));
+
+      System.out.println("\nTURN 5");
+      System.out.println(p1.support());
+      System.out.println(p2.specialAttack(p1));
+
+  
+      if (p1.getHP() <= 0 && p2.getHP() <= 0){
+        System.out.println("\nBoth players have died. Whomp whomp :(");
+        return;
+      }
+      else
+        if (p1.getHP() <= 0){
+          System.out.println("\n" + p2.getName() + " has won the duel!");
+          return;
+        }
+        else{
+          System.out.println("\n" + p1.getName() + " has won the duel!");
+          return;}
     }
 }
+
