2e2c4327494087608701d732c1842763ce720223
Sean Takahashi <seanyuto@gmail.com>
2024-12-09 23:05:14 -0500

    changed so that support methods do not exceed maxHP and act accordingly in each case

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -44,21 +44,45 @@ public class Warrior extends Adventurer{
   //hurt or hinder the target adventurer
   public String attack(Adventurer other){
     other.applyDamage(5);
-    return getName() + " attacked " + other.getName() + " and applied 5 damage";
+    return getName() + " attacked " + other.getName() + " and applied 5 damage.";
   }
 
   //heal or buff the target adventurer
   public String support(Adventurer other){
-    other.applyHeal(5);
-    return getName() + " supported " + other.getName() + " by healing 5 HP";
+    if(!(other.getmaxHP() - other.getHP() <= 5)){
+      other.applyHeal(5);
+      return getName() + " supported " + other.getName() + " by healing 5 HP.";  
+    }
+    else if(this.getmaxHP() > this.getHP()){
+      int diff = this.getmaxHP() - this.getHP();
+      applyHeal(diff);
+      return getName() + " supported " + other.getName() + " by healing " + diff + "HP.";  
+    }
+    else{
+      return "Uh oh, " + other.getName() + " already is at max HP. Therefore, no action was taken.";  
+    }
   }
 
   //heal or buff self
   public String support(){
-    applyHeal(5);
-    int spec = (int)(5 * Math.random() + 1);
-    restoreSpecial(spec);
-    return getName() + " supported themself by healing 5 HP and gained " + spec + " mana";
+    if(!(this.getmaxHP() - this.getHP() <= 5)){
+      applyHeal(5);
+      int spec = (int)(5 * Math.random() + 1);
+      restoreSpecial(spec);
+      return getName() + " supported themself by healing 5 HP and gained " + spec + " mana.";  
+    }
+    else if(this.getmaxHP() > this.getHP()){
+      int diff = this.getmaxHP() - this.getHP();
+      applyHeal(diff);
+      int spec = (int)(5 * Math.random() + 1);
+      restoreSpecial(spec);
+      return getName() + " supported themself by healing " + diff + "HP and gained " + spec + " mana.";  
+    }
+    else{
+      int spec = (int)(5 * Math.random() + 1);
+      restoreSpecial(spec);
+      return "Uh oh, " + getName() + " already is at max HP. Instead of healing, " + getName() + " only gained " + spec + " mana from the process.";  
+    }
   }
 
   //hurt or hinder the target adventurer, consume some special resource
@@ -66,7 +90,7 @@ public class Warrior extends Adventurer{
     if (this.mana >= 5){
       this.mana -= 5;
       other.applyDamage(20);
-      return getName() + " used their special attack on " + other.getName() + " and dealt 20 damage, consumed 5 mana";
+      return getName() + " used their special attack on " + other.getName() + " and dealt 20 damage, consumed 5 mana.";
     }
     else{
       applyDamage(5);
